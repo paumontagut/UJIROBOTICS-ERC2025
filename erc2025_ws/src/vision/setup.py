22031f827431e4cadd3a70e2dda3fd2d0150664f
diegomarzaa <al426641@uji.es>
2025-03-11 14:22:16 +0100

    CI Cache Error Test

--- erc2025_ws/src/vision/setup.py
+++ erc2025_ws/src/vision/setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 package_name = 'vision'
 
 setup(
-    name=package_name,
+    name=package_name,ñ
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
