231fec0f02f9217d952345890057573df7b5b2ec
diegomarzaa <al426641@uji.es>
2025-03-11 14:24:39 +0100

    CI Cache Error Fix

--- erc2025_ws/src/vision/setup.py
+++ erc2025_ws/src/vision/setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 package_name = 'vision'
 
 setup(
-    name=package_name,ñ
+    name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
